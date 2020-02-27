9432b2128543d5ba8d7bcaf056992cd8d88c77fb
parisadj72 <37019391+parisadj72@users.noreply.github.com>
2020-02-27 14:01:41 -0500

    Add files via upload

---
+++ Poem.pl
@@ -0,0 +1,26 @@
+:- dynamic gharn/2.
+
+gharn(khayam, 5).
+gharn(attar, 8).
+gharn(jami, 9).
+gharn(nezami, 5).
+gharn(molavi, 7).
+gharn('\u0633\u0639\u062F\u06CC', 8).
+
+:- dynamic sabk/2.
+
+sabk(nezami, ghaside).
+sabk(khayam, robaei).
+sabk(molavi, masnavi).
+sabk('\u0633\u0639\u062F\u06CC', '\u0642\u0635\u06CC\u062F\u0647').
+
+:- dynamic asar/2.
+
+asar(jami, baharestan).
+asar(nezami, haft_peykar).
+asar(nezami, leyliOmajnun).
+asar(nezami, eskandarname).
+asar(molavi, masnavi_manavi).
+asar('\u0633\u0639\u062F\u06CC', '\u06AF\u0644\u0633\u062A\u0627\u0646').
+asar(nezami, khosro_o_shirin).
+
